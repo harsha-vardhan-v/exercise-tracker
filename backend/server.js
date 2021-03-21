6c949f3d94809772ce5348f3981f1d9d3f517b71
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-03-21 20:18:08 +0530

    Connected the server with MongoDB

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose'); //To connect with mongoDB
+
+//Configures environment variables in the .env file
+require('dotenv').config();
+
+//Creates the express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+//COnfigures the app to use json
+app.use(cors());
+app.use(express.json());
+
+//Connect with MongoDB atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection established successfully')
+});
+
+//Starts the app
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
