a5fc1c490f48e04d3283bf1447473e4a31283918
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-03-23 18:48:14 +0530

    Added base components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navbar from './components/navbar.component';
+import ExerciseList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.components';
+
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+
+      <Route path = "/" exact component = {ExerciseList} />
+      <Route path = "/exercise/edit/:id" component = {EditExercise} />
+      <Route path = "/exercise/create" component = {CreateExercise} />
+      <Route path = "/user/create" component = {CreateUser} />
+    </Router>
   );
 }
 
